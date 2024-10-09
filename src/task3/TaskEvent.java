4945c1a8340cd5272e8536664a7fc78659c466bb
SSSSSSissssssss <siddvael@gmail.com>
2024-10-09 09:04:24 +0200

    task3

---
+++ src/task3/TaskEvent.java
@@ -0,0 +1,21 @@
+package task3;
+import task1.*;
+import task2.*;
+
+class TaskEvent {
+    
+    void post(Runnable r){
+        //TODO
+    };
+    static TaskEvent taskE(){
+        //TODO;
+        return new TaskEvent();
+    };
+    void kill(){
+        //TODO
+    };
+    boolean killed(){
+        //TODO
+        return true;
+    };
+    }
